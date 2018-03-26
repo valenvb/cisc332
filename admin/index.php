299bf0277752b4a3b648fc592a373ed02f73cadb
Valen Varangu Booth <valen@valenvb.ca>
2018-03-26 03:59:06 -0400

    adding UI

--- admin/index.php
+++ admin/index.php
@@ -86,7 +86,7 @@ if ( $_SESSION['user_type'] != "A" ){
             <h1 class="h2">Movies</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
               <div class="btn-group mr-2">
-                <button class="btn btn-sm btn-outline-secondary">Add</button>
+                <button class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#addMovieModal" > Add</button>
               </div>
 
             </div>
@@ -101,13 +101,13 @@ if ( $_SESSION['user_type'] != "A" ){
                   <th>Runtime</th>
                   <th>Rating</th>
                   <th>Release Date</th>
-                  <th>Action</th>
+                  
                 </tr>
               </thead>
               <tbody>
 <?php 
 
-include '../lib/database.php';
+include_once '../lib/database.php';
 $movies = $db->query('SELECT * from Movie');
 
 while($movie = $movies->fetch()){
@@ -121,6 +121,47 @@ while($movie = $movies->fetch()){
           </div>
           </div> <!--END MOVIE TABLE-->
 
+
+
+          <div id="show_list">
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 ">
+            <h1 class="h2">Showings</h1>
+            <div class="btn-toolbar mb-2 mb-md-0">
+              <div class="btn-group mr-2">
+                <button class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#addShowingModal"> Add</button>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>Movie</th>
+                  <th>Complex</th>
+                  <th>Theater</th>
+                  <th>Time</th>
+                  <th>Date</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+<?php 
+
+include_once '../lib/database.php';
+$shows = $db->query("select M.title, C.`name`, S.STIME, S.SDATE, theaterno from showing S, movie M, complex C where S.complexNo=C.complexNo and S.movieID=M.movieid");
+//print($users->queryString);
+while($show = $shows->fetch()){
+  echo "<tr><td>".$show['title']."</td><td>".$show["name"]."</td><td>".$show["theaterno"]."</td><td>".$show["STIME"]."</td><td>".$show["SDATE"]."</td><td><a href=\"actions.php?action=ds&title=".$show['title']."&name=".$show['name']."&stime=".$show['STIME']."&sdate=".$show['SDATE']."\" class=\"btn btn-danger\">Delete</a></td></tr>";
+};
+
+?>              
+                
+              </tbody>
+            </table>
+          </div>
+          </div> <!--END show TABLE-->
 
 
           <div id="cust_list">
@@ -143,7 +184,7 @@ while($movie = $movies->fetch()){
               <tbody>
 <?php 
 
-include '../lib/database.php';
+include_once '../lib/database.php';
 $users = $db->query("SELECT * from Users, Member where Users.userid=member.userid");
 //print($users->queryString);
 while($user = $users->fetch()){
@@ -156,26 +197,182 @@ while($user = $users->fetch()){
             </table>
           </div>
           </div> <!--END User TABLE-->
+
+
+
+
         </main>
       </div>
     </div>
 
-    <div class="modal  fade" id="iframeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal  fade" id="iframeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Customer Reservations</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <iframe id="modalIframe" src="" frameborder="0" style="width:90%;" ></iframe>
+        <iframe id="modalIframe" src="" frameborder="0" style="width:100%;"></iframe>
       </div>
     </div>
   </div>
 </div>
     
+
+<div class="modal  fade" id="addShowingModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add a Showtime</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="actions.php?action=addShow" method="POST">
+        <div class="form-group">
+            <label for="title">Movie</label>
+            <select name="title" class="custom-select" type="text">
+
+              <?php 
+                $mvs = $db->query("select title, movieid from movie");
+                while($mv=$mvs->fetch()){
+                  echo"<option value='".$mv['movieid']."'>".$mv['title']."</option>";
+                }
+              ?>
+
+            </select>
+        </div>
+
+        <div class="form-row">
+
+          <div class="col form-group">
+            <label for="where">Complex</label>
+            <select name="where" class="custom-select" type="text">
+
+              <?php 
+                $cmplxs = $db->query("select name, complexNo from complex");
+                while($cmplx=$cmplxs->fetch()){
+                  echo"<option value='".$cmplx['complexNo']."'>".$cmplx['name']."</option>";
+                }
+              ?>
+
+            </select>
+          </div>
+          <div class="col form-group">
+              <label for="whereT">Theater No.</label>
+              <input name="whereT" min=0 class="form-control" type="text">
+          </div>
+              
+        </div>
+
+        <div class="form-row">
+          <div class="col form-group">
+              <label for="sdate">Date</label>
+              <input name="sdate" class="form-control" type="date">
+          </div>
+          <div class="col form-group">
+              <label for="stime">Time</label>
+              <input name="stime" class="form-control" type="time">
+          </div>
+        </div>
+
+        <button class="btn btn-primary right" type="submit">Add</button>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal  fade" id="addMovieModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add a Movie</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="actions.php?action=add" method="POST">
+          <div class="form-group">
+            <label for="title">Movie Title</label>
+            <input name="title" class="form-control" type="text">
+          </div>
+          <div class="form-group">
+            <label for="director">Director</label>
+            <input name="director" class="form-control" type="text">
+          </div>
+
+          <div class="form-group">
+            <label for="plot">Plot</label>
+            <textarea name="plot" rows=2 class="form-control" type="text"></textarea>
+          </div>
+
+          <div class="form-row">
+          <div class="col form-group">
+            <label for="runtime">Runtime</label>
+            <input name="runtime" min=0 class="form-control" type="number">
+          </div>
+          <div class="col form-group">
+            <label for="rating">Rating</label>
+            <select name="rating" class="custom-select form-control">
+              <option>G</option>
+              <option>PG</option>
+              <option>14A</option>
+              <option>18A</option>
+              <option>A</option>
+              <option>R</option>
+            </select>
+          </div>
+          </div>
+
+          <div class="form-group">
+            <label for="producer">Producer</label>
+            <input name="producer" class="form-control" type="text">
+          </div>
+
+          <div class="form-row">
+          <div class="col form-group">
+            <label for="sdate">Start Date</label>
+            <input name="sdate" class="form-control" type="date">
+          </div>
+          <div class="col form-group">
+            <label for="edate">End Date</label>
+            <input name="edate" class="form-control" type="date">
+          </div>
+          </div>
+
+          <div class="form-group">
+            <label for="supplier">Supplier</label>
+            <select name="supplier" class="custom-select form-control">
+              <?php 
+                $suppliers = $db->query("select `Name`, supplierid as id  from supplier");
+
+                while($sup=$suppliers->fetch()){
+                  echo"<option value='".$sup['id']."'>".$sup['Name']."</option>";
+                }
+              ?>
+            </select>
+          </div>
+
+          <div class="form-group">
+            <label for="cast">Cast</label><br>
+            <small class"form-text text-muted">Each on a new line</small>
+            <textarea name="cast" rows=2 class="form-control" type="text"></textarea>
+          </div>
+
+
+          <button class="btn btn-primary right" type="submit">Add</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
